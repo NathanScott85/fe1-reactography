f9bd22b6103c65fb8cd28894c915ae6b4703f6de
Kurtis Angell <KurtisBlow@MacBook-Pro.local>
2018-11-06 17:06:35 +0000

    second

--- src/App.js
+++ src/App.js
@@ -1,39 +1,65 @@
 import React, { Component } from 'react';
 import Events from './components/Events'
 import CategorySelector from './components/CategorySelector'
+import Search from './components/Search'
 import Axios from '../node_modules/axios';
+
 
 class App extends Component {
   state = {
-    events: []
+    events: [],
+    search: '',
+    result: ''
   }
 
   render() {
     return (
       <main>
         <h1>EVENTS</h1>
-        < CategorySelector />
+        <input placeholder="Enter City" value={this.state.search} onChange={this.onInputChange} />
+        <button onClick={this.onSubmit} > SUBMIT </button>
+        <br />
+        <CategorySelector />
+
+        {/* filter below  */}
         {this.state.events.length && < Events events={this.state.events} />}
       </main>
     )
   }
 
+  onInputChange = event => {
+    this.setState({
+      search: event.target.value
+    })
+  }
+
+  onSubmit = () => {
+    this.setState({
+      result: this.state.search, search: ''
+    })
+  }
+
   componentDidMount = () => {
-    Axios.get('https://app.ticketmaster.com/discovery/v2/events.json?city=Leeds&apikey=oFolx3SJL91d7LQG0WsmPzXZKIGWXwee')
+    Axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?size=5&countryCode=GB&apikey=oFolx3SJL91d7LQG0WsmPzXZKIGWXwee`)
       .then(result => {
-        console.log(result)
-        // console.log(Object.keys(res.data))
-        // console.log(result.data._embedded)
+        //manipulate the data with a reduce into a new array of objects 
+        //pass into the state
+
+        const data = [...result.data._embedded.events]
+        const eventData = data.reduce((acc, val, i, array) => {
+          acc[i] = {
+            name: val.name,
+            genre: val.classifications[0].segment.name,
+            venue: val._embedded.venues[0].name
+          }
+          return acc;
+        }, [])
         this.setState({
-          // events: result.data._embedded.events
-          events: result.data
+          events: eventData
         })
       })
       .catch('failed')
   }
-
-
-
 }
 
 export default App;
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,20 +1,11 @@
 import React from 'react'
 
-const Events = ({ events }) => {
-    // console.log(events)
+const Events = ({ events, }) => {
     return (
-        <ul>
-            {events.map((event, index) => {
-                return (
-                    <li key={index} >
-                        {Object.keys(event)}
-                        {console.log(event.classifications[0].segment.name)}
-                    </li>
-                )
-            })}
-        </ul>
+        <ul> {events.map(event => {
+            return (<li>{event.name} </li>)
+        })} </ul>
     )
-
 }
 
 
